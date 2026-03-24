07e264ccc9ff4ee54564a95aae66dd3d93e2f4dc
Maksim Maltsev <maksimmaltsev@Maksims-MacBook-Air-2.local>
2026-03-24 09:47:48 +0300

    refactor(bot): separate handlers

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,8 +1,7 @@
 """Command handlers for the LMS Telegram Bot."""
 
+from .basic import handle_start, handle_help
 from .commands import (
-    handle_start,
-    handle_help,
     handle_health,
     handle_labs,
     handle_scores,
