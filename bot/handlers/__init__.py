d0bb87f846fca1abcbe6c94fb511c1d3d35fae3d
Maksim Maltsev <maksimmaltsev@Maksims-MacBook-Air-2.local>
2026-03-24 09:57:02 +0300

    refactor(bot): split handlers into folders

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,11 +1,7 @@
 """Command handlers for the LMS Telegram Bot."""
 
 from .basic import handle_start, handle_help
-from .commands import (
-    handle_health,
-    handle_labs,
-    handle_scores,
-)
+from .commands import handle_health, handle_labs, handle_scores
 
 __all__ = [
     "handle_start",
